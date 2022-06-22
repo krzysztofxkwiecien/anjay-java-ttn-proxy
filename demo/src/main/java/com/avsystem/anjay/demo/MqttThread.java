3a7dc64a3ea2d5bfe1ede81c5736799ae0c0ff0b
Arflayd <krzysztofxkwiecien@gmail.com>
2022-06-22 21:53:02 +0200

    Mqtt credentials read from file

--- demo/src/main/java/com/avsystem/anjay/demo/MqttThread.java
+++ demo/src/main/java/com/avsystem/anjay/demo/MqttThread.java
@@ -1,8 +1,17 @@
 package com.avsystem.anjay.demo;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import org.eclipse.paho.client.mqttv3.MqttClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
 import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class MqttThread extends Thread {
 
@@ -10,11 +19,34 @@ public class MqttThread extends Thread {
 
     public void run() {
 
+        String content = "";
+
         try {
+            content = Files.readString(Path.of("mqtt_credentials.json"));
+        }
+        catch(IOException e){
+            System.err.println("Error opening MQTT credentials file");
+            System.exit(-1);
+        }
+
+        String username = "";
+        String password = "";
+
+        try {
+            JsonObject json = new Gson().fromJson(content, JsonObject.class);
+            username = json.get("username").toString();
+            password = json.get("password").toString();
+        }catch(Exception e){
+            System.err.println("Error reading MQTT credentials file");
+            System.exit(-1);
+        }
+
+        try {
+            Gson gson = new Gson();
             MqttClient client = new MqttClient("tcp://eu1.cloud.thethings.network:1883", "JavaClient");
             MqttConnectOptions options = new MqttConnectOptions();
-            options.setUserName("");
-            options.setPassword("".toCharArray());
+            options.setUserName("end-device-test-1@ttn");
+            options.setPassword("NNSXS.4YLFQ2Q3QMZHKFTZMU5AUEUEVWJPZPAB3SIDEGQ.XSZMTQDKXAMDV2FTXCPSLNAVFLLH442BTDYLEKPXF4SKPCMQ2ZUA".toCharArray());
             client.connect(options);
             System.out.println("MQTT Client Start");
             MqttMessageSender.client = client;
